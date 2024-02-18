4774bf80c8d8f1aabe14a0f36ebafb899767bfed
eunice <eunicewanjiru208@gmail.com>
2024-02-18 23:25:23 +0300

    Prompt the user to enter chemistry marks and store the input in the 'chemistry' variable.

--- average.java
+++ average.java
@@ -12,8 +12,8 @@ double math, biology, chemistry, english, swahili, total,average;
  System.out.println("Enter biology marks:");
   biology = eunice.nextInt();
 
-// System.out.println("Enter chemistry marks:");
-//  chemistry = eunice.nextInt();
+ System.out.println("Enter chemistry marks:");
+  chemistry = eunice.nextInt();
 
 // System.out.println("Enter english marks:");
 //  english = eunice.nextInt();
